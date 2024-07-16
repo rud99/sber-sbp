48ac5bff1f38d612517579189d8c8d4337ac4303
Mikhail Zakharov <mz-83@mail.ru>
2024-07-16 14:59:23 +0400

    init commit

---
+++ src/Client.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rud99\SberSbp {
+
+    class Client
+    {
+        public function greet(String $sName)
+        {
+            return 'Hi ' . $sName . '! How are you doing today?';
+        }
+    }
+}
+
